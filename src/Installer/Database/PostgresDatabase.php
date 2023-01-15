441b10cd7c129b22df30cbca8fdb83d6f9953250
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2023-01-15 17:43:37 +0100

    Better handling of the postgress installation
    In some cases we aren't using any host and, instead
    use socket connections. Better support that.

--- src/Installer/Database/PostgresDatabase.php
+++ src/Installer/Database/PostgresDatabase.php
@@ -45,25 +45,36 @@ class PostgresDatabase extends AbstractDatabase
             }
         }
 
-        $cmd = [];
+        $passcmd = [];
         if (!empty($this->pass)) {
-            $cmd = [
+            $passcmd = [
                 'env',
-                'PGPASSWORD=' . $this->pass];
+                'PGPASSWORD=' . $this->pass,
+            ];
         }
 
-        $cmd = array_merge($cmd, [
-            'psql',
-            '-c',
-            sprintf('CREATE DATABASE "%s";', $this->name),
-            '-U',
-            $this->user,
-            '-d',
-            'postgres',
-            '-h',
-            $this->host,
-            !empty($this->port) ? ' --port ' . $this->port : '',
-        ]);
+        $hostcmd = [];
+        if (!empty($this->host)) {
+            $hostcmd = [
+                '-h',
+                $this->host,
+            ];
+        }
+
+        $cmd = array_merge(
+            $passcmd,
+            [
+                'psql',
+                '-c',
+                sprintf('CREATE DATABASE "%s";', $this->name),
+                '-U',
+                $this->user,
+                '-d',
+                'postgres',
+                !empty($this->port) ? ' --port ' . $this->port : '',
+            ],
+            $hostcmd
+        );
 
         return array_filter($cmd); // Remove empties.
     }
